351bad64af5a61a5f59e5790bcdf03acf36ce276
Afif-20 <fadlurrahman.afif23@gmail.com>
2025-08-29 16:57:04 +0700

    menambahkan isi

--- resources/views/user/components/navbar.blade.php
+++ resources/views/user/components/navbar.blade.php
@@ -1,4 +1,4 @@
-  <nav class="navbar navbar-dark navbar-expand-lg" style="background-color:#00D4E7 ">
+  <nav class="navbar navbar-dark navbar-expand-lg" style="background-color:#00D4E7">
       <div class="container">
           <a class="navbar-brand" href="#">
               <h3>Vina Collection</h3>
@@ -7,66 +7,43 @@
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end d-flex " id="navbarSupportedContent">
-              <ul class="navbar-nav gap-4 ">
+
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav gap-4 align-items-center">
                   <li class="nav-item">
-                      <a class="nav-link fs-5  {{ Request::path() == '/' ? 'active' : '' }}" aria-current="page"
-                          href="/">
-                          Home
-                      </a>
+                      <a class="nav-link fs-5 {{ Request::path() == '/' ? 'active' : '' }}" href="/">Home</a>
                   </li>
                   <li class="nav-item">
                       <a class="nav-link fs-5 {{ Request::path() == 'statuspesanan' ? 'active' : '' }}"
-                          aria-current="page" href="/statuspesanan">
-                          Staus Pesanan
-                      </a>
+                          href="/statuspesanan">Status Pesanan</a>
                   </li>
-                  <div class="d-flex gap-4 align-items-center">
-                      {{-- Kalau belum login --}}
-                      @guest
+
+                  @guest
+                      <li class="nav-item">
                           <a href="{{ route('login') }}">
                               <button type="button" class="btn btn-outline-light">Login | Register</button>
                           </a>
-                      @endguest
+                      </li>
+                  @endguest
 
-                      {{-- Kalau sudah login --}}
-                      @auth
+                  @auth
+                      <li class="nav-item">
                           <form method="POST" action="{{ route('logout') }}">
                               @csrf
                               <button type="submit" class="btn btn-outline-light">Logout</button>
                           </form>
-                      @endauth
+                      </li>
+                  @endauth
 
+                  <li class="nav-item">
                       <div class="notif">
-                          <a href="/cart" class="fs-4">
-                              <i class="fa-solid icon-nav fa-bag-shopping "></i>
+                          <a href="/cart" class="fs-4 nav-link {{ Request::path() == 'cart' ? 'active' : '' }}">
+                              <i class="fa-solid fa-bag-shopping"></i>
                           </a>
                           <div class="circle">10</div>
                       </div>
-                  </div>
-                  {{-- </li>
-                  <li class="nav-item">
-                      <button type="button"  class="btn btn-success" data-bs-toggle="modal"
-                          data-bs-target="#exampleModal">
-                          Login </button>
-                  </li> --}}
-                  {{-- <li class="nav-item">
-                      <div class="notif">
-                          <a href="/transaksi"
-                              class="fs-5 nav-link {{ Request::path() == 'transaksi' ? 'active' : '' }}">
-                              <i class="fa fa-bag-shopping"></i>
-                          </a>
-                          @if ($count)
-                              <div class="circle">{{ $count }}</div>
-                          @endif
-                      </div>
-                  </li> --}}
+                  </li>
               </ul>
-              {{-- <div class="d-flex gap-4 align-items-center">
-                  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                      Login | Register</button>
-
-              </div> --}}
           </div>
       </div>
   </nav>
