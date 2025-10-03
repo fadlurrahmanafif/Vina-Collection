f69914424eb0b9b05c3d57546f36ca6128e7f013
Afif-20 <fadlurrahman.afif23@gmail.com>
2025-10-03 12:58:57 +0700

    menambah file action delete untuk aksi delete pesanan

---
+++ app/Actions/DeleteOrderAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Actions;
+
+use App\Services\DataOrderService;
+
+readonly class DeleteOrderAction
+{
+    public function __construct(
+        private readonly DataOrderService $dataOrderService,
+    ) {}
+
+    public function execute(int $orderId)
+    {
+        $this->dataOrderService->deleteOrder($orderId);
+    }
+}
